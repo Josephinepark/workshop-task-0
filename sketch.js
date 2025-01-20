4f3e6d4397910458694dbfb9440b0f99e1e53398
Josephine <jpar693@aucklanduni.ac.nz>
2025-01-20 18:49:23 +1300

    Added variable and conditional

--- sketch.js
+++ sketch.js
@@ -4,15 +4,23 @@ function setup() {
 
 function draw() {
   background(0, 0, 100);
-  
-  fill(0,0,0)
-  stroke(100,100,200)
-  strokeWeight(10)
-  rect(290,150,25,75)
-  
-  line(0,0,400,600)
-  
-  circle(80,20,30,20)
-  
-  ellipse(200,150,90,80)
+
+  let x = 0;
+  fill(x, x, x);
+  y = random(100);
+
+  if (y > 50) {
+    stroke(100, 100, 200);
+  } else {
+    stroke(200, 200, 100);
+  }
+
+  strokeWeight(10);
+  rect(290, 150, 25, 75);
+
+  line(0, 0, 400, 600);
+
+  circle(80, 20, 30, 20);
+
+  ellipse(200, 150, 90, 80);
 }
