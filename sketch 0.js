9a580307d23797f2864c42fd4a5a47bc736098c6
Josephine <jpar693@aucklanduni.ac.nz>
2025-01-20 18:19:52 +1300

    replace sketch file

---
+++ sketch 0.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0, 0, 100);
+  
+  fill(0,0,0)
+  stroke(100,100,200)
+  strokeWeight(10)
+  rect(290,150,25,75)
+  
+  line(0,0,400,600)
+  
+  circle(80,20,30,20)
+  
+  ellipse(200,150,90,80)
+}
